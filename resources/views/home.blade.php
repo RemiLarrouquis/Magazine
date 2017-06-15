3ea8e604dd8c436abaa057fde06033d974a49ae1
rlarrouq <remi.larrouquis@gmail.com>
2017-06-15 09:26:04 +0200

    Ajout du style css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-
-                @foreach ($publications as $publication)
-                    <img src="{{url('uploads/'.$publication->fichier->nom_fichier)}}" alt="test">
-                    <h4>{{ $publication->titre }}</h4>
-                    <p>{!! nl2br($publication->description) !!}</p>
-                @endforeach
-            </div>
-        </div>
+        @include('publication.cards');
     </div>
 </div>
 @endsection
--- resources/views/nav/navbar.blade.php
+++ resources/views/nav/navbar.blade.php
@@ -1,67 +1,45 @@
-<nav class="navbar navbar-default navbar-static-top">
+<nav class="navbar navbar-inverse bg-inverse navbar-toggleable-md sticky-top navbar-light bg-faded">
     <div class="container">
         <div class="navbar-header">
 
-            <!-- Collapsed Hamburger -->
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                <span class="sr-only">Toggle Navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-
             <!-- Branding Image -->
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
-            </a>
+            <a class="navbar-brand page-scroll" href="{{ url('/home') }}">Publi'Magazine</a>
         </div>
 
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
             <!-- Left Side Of Navbar -->
-            <ul class="nav navbar-nav">
-                &nbsp;
+            <ul class="navbar-nav mr-auto">
+                @if (!Auth::guest())
+                    <li class="nav-item">
+                        <a class="nav-link page-scroll" href="{{ url('/publication') }}">Publications</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link page-scroll" href="{{ url('/client') }}">Clients</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link page-scroll" href="{{ url('/story') }}">Historiques</a>
+                    </li>
+                @endif
             </ul>
 
             <!-- Right Side Of Navbar -->
-            <ul class="nav navbar-nav navbar-right">
+            <ul class="navbar-nav">
                 <!-- Authentication Links -->
                 @if (Auth::guest())
-                    <li><a href="{{ route('login') }}">Login</a></li>
-                    <li><a href="{{ route('register') }}">Register</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
                 @else
-
-
-                    <li class="hidden">
-                        <a href="{{ url('/home') }}"></a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="{{ url('/publication') }}">Publications</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="{{ url('/client') }}">Clients</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="{{ url('/story') }}">Historiques</a>
-                    </li>
-
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                    <li class="nav-item dropdown show">
+                        <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
                             {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
-
-                        <ul class="dropdown-menu" role="menu">
-                            <li>
-                                <a href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    Se déconnecter
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    {{ csrf_field() }}
-                                </form>
-                            </li>
-                        </ul>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdown02">
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                Se déconnecter
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                        </div>
                     </li>
                 @endif
             </ul>
