6ba3abe3f6183af39b88e67a0c1e56382834eeb2
rlarrouq <remi.larrouquis@gmail.com>
2017-07-07 08:54:22 +0200

    Pour liste des impayé seule les en cours (date)

--- resources/views/historique/form.blade.php
+++ resources/views/historique/form.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.home')
 
 @section('content')
-    <article class="content dashboard-page">
+    <article class="content dashboard-page liste-historique">
         <div class="title-block">
             <h3 class="title"> Historiques </h3>
             <p class="title-description"> {{ $sub_title }} </p>
