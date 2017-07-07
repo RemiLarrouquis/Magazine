02e0cdea5e3edc9ee5e9e7abc6719a1d7281e445
rlarrouq <remi.larrouquis@gmail.com>
2017-07-07 09:38:52 +0200

    correction

--- resources/views/nav/sidebar.blade.php
+++ resources/views/nav/sidebar.blade.php
@@ -39,7 +39,7 @@
                     </a>
                 </li>
                 <li>
-                    <a id="liste-abonnement-impaye" href="{{ url('/abonnement/list?full=true&filterPaye=8&filterEnCours=false') }}">
+                    <a id="liste-abonnement-impaye" href="{{ url('/abonnement/list?full=true&filterPaye=8&filterEnCours=true') }}">
                         Impayées
                     </a>
                 </li>
